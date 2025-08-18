1e01a008d7eb1fc5e2ced7c7aae3959a7d6d6c44
Gregory Buchenberger <buchenberger@gmail.com>
2025-08-17 21:30:50 -0600

    it builds

--- src/main/java/org/eightfoldconsulting/nimas2pdf/FrameImageResize.java
+++ src/main/java/org/eightfoldconsulting/nimas2pdf/FrameImageResize.java
@@ -2,34 +2,91 @@ package org.eightfoldconsulting.nimas2pdf;
 
 import javax.swing.*;
 import java.awt.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.logging.Logger;
 
 public class FrameImageResize extends JDialog {
-    private static final Logger logger = LoggerFactory.getLogger(FrameImageResize.class);
+    private static final Logger logger = Logger.getLogger(FrameImageResize.class.getName());
     
     private JSpinner spinnerDPI;
     private JSpinner spinnerMaxWidth;
     private JSpinner spinnerMaxHeight;
     private JButton btnOK;
     private JButton btnCancel;
+    
     private boolean approved = false;
 
     public FrameImageResize(Frame parent) {
-        super(parent, "Image Settings", true);
+        super(parent, true);
         initComponents();
         setupActions();
         setLocationRelativeTo(parent);
     }
 
-    // ...existing code...
+    private void initComponents() {
+        spinnerDPI = new JSpinner(new SpinnerNumberModel(300, 72, 1200, 1));
+        spinnerMaxWidth = new JSpinner(new SpinnerNumberModel(800, 100, 5000, 10));
+        spinnerMaxHeight = new JSpinner(new SpinnerNumberModel(1000, 100, 5000, 10));
+        btnOK = new JButton("OK");
+        btnCancel = new JButton("Cancel");
+
+        JPanel contentPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        
+        // Add components using GridBagLayout
+        gbc.gridx = 0; gbc.gridy = 0;
+        contentPanel.add(new JLabel("DPI:"), gbc);
+        
+        gbc.gridx = 1;
+        contentPanel.add(spinnerDPI, gbc);
+        
+        gbc.gridx = 0; gbc.gridy = 1;
+        contentPanel.add(new JLabel("Max Width:"), gbc);
+        
+        gbc.gridx = 1;
+        contentPanel.add(spinnerMaxWidth, gbc);
+        
+        gbc.gridx = 0; gbc.gridy = 2;
+        contentPanel.add(new JLabel("Max Height:"), gbc);
+        
+        gbc.gridx = 1;
+        contentPanel.add(spinnerMaxHeight, gbc);
+        
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(btnOK);
+        buttonPanel.add(btnCancel);
+        
+        setLayout(new BorderLayout());
+        add(contentPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+        
+        pack();
+    }
+
+    private void setupActions() {
+        btnOK.addActionListener(e -> {
+            approved = true;
+            dispose();
+        });
+        
+        btnCancel.addActionListener(e -> dispose());
+        
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                approved = false;
+                dispose();
+            }
+        });
+    }
 
     public boolean showDialog() {
         setVisible(true);
         return approved;
     }
 
-    // Getter methods
     public int getDPI() {
         return (Integer) spinnerDPI.getValue();
     }
